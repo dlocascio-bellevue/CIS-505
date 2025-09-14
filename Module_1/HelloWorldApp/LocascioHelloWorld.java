0de97a607817a689c83c304d967ff7e7ed0bd6ae
Danny <dlocascio@my365.bellevue.edu>
2025-09-13 22:24:00 -0400

    Added LocascioHelloWorld program for Module_1.

---
+++ Module_1/HelloWorldApp/LocascioHelloWorld.java
@@ -0,0 +1,11 @@
+// LocascioHelloWorld takes two String variables and produces a singular String output
+
+public class LocascioHelloWorld {
+
+    public static void main(String[] args) {
+        String firstName = "Danny";
+        String lastName = "Locascio";
+
+        System.out.printf ("Hello World from %s %s.", firstName, lastName);
+    }
+}
